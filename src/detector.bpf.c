9e2ed4fd0112c565c48c0ff8a39c127668bfee6a
elijahud <udomelijah@protonmail.com>
2025-03-17 19:43:44 +0100

    Feat:Added debugger

--- src/detector.bpf.c
+++ src/detector.bpf.c
@@ -6,6 +6,8 @@ int detect_container_escape(struct pt_regs *ctx) {
     char comm[16];
     bpf_get_current_comm(&comm, sizeof(comm));
 
+bpf_printk("EXECVE: %s", comm);
+
     // Combined check for docker and unshare
     if ((comm[0] == 'd' && comm[1] == 'o' && comm[2] == 'c') ||  // docker
         (comm[0] == 'u' && comm[1] == 'n' && comm[2] == 's')) {  // unshare
