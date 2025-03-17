162d09769616145595efc9da62978e0b56bef35f
elijahud <udomelijah@protonmail.com>
2025-03-17 20:15:26 +0100

    Fix:

--- src/detector.bpf.c
+++ src/detector.bpf.c
@@ -7,14 +7,11 @@ int detect_container_escape(struct pt_regs *ctx) {
     char comm[16];
     bpf_get_current_comm(&comm, sizeof(comm));
 
-bpf_printk("EXECVE: %s", comm);
-bpf_printk("BPF_DEBUG: %s", comm); 
-
-    // Combined check for docker and unshare
-      if ((comm[0] == 'c' && comm[1] == 'o' && comm[2] == 'n') || // containerd
-       (comm[0] == 'r' && comm[1] == 'u' && comm[2] == 'n')) {  // runc
-        bpf_printk("ALERT: %s", comm);
-}
+    // Detect container runtime components
+    if ((comm[0] == 'c' && comm[1] == 'o' && comm[2] == 'n') ||  // containerd
+        (comm[0] == 'r' && comm[1] == 'u' && comm[2] == 'n')) {  // runc
+        bpf_printk("ALERT: Container escape via %s", comm);
+    }
     return 0;
 }
 
