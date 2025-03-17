ceb7d0afb5f85d3ce335a52181d3b27ecf5fb1f0
elijahud <udomelijah@protonmail.com>
2025-03-17 19:37:49 +0100

    Fix:Update

--- src/detector.bpf.c
+++ src/detector.bpf.c
@@ -5,10 +5,11 @@ SEC("tracepoint/syscalls/sys_enter_execve")
 int detect_container_escape(struct pt_regs *ctx) {
     char comm[16];
     bpf_get_current_comm(&comm, sizeof(comm));
-    
-    // Detect container-related processes
-    if (comm[0] == 'd' && comm[1] == 'o' && comm[2] == 'c') {
-        bpf_printk("Container escape attempt: %s", comm);
+
+    // Combined check for docker and unshare
+    if ((comm[0] == 'd' && comm[1] == 'o' && comm[2] == 'c') ||  // docker
+        (comm[0] == 'u' && comm[1] == 'n' && comm[2] == 's')) {  // unshare
+        bpf_printk("Container escape detected: %s", comm);
     }
     return 0;
 }
